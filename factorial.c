b8e01384555d1ee444633c0b58989a8ca20f55bc
novez <novez_1@hotmail.com>
2015-04-24 20:42:21 +0700

    Second Version of 5 files

--- factorial.c
+++ factorial.c
@@ -3,13 +3,13 @@
 int main()
 {
   int c, n, fact = 1;
- 
+  printf("User-B add this line for problem18 [time 1]\n");
   printf("Enter a number to calculate it's factorial\n");
   scanf("%d", &n);
  
   for (c = 1; c <= n; c++)
     fact = fact * c;
- 
+  printf("User-B add this line for problem18 [time 2]\n");
   printf("Factorial of %d is %d\n", n, fact);
  
   return 0;
